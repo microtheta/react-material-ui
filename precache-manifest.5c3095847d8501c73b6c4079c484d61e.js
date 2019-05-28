a9fff7094947ff2d4e990768f898c4396911416c
Mahesh <mahesh@macbook-pro.local>
2019-05-28 12:17:49 +0530

    Updates

---
+++ precache-manifest.5c3095847d8501c73b6c4079c484d61e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "331c89219a5545eeb06518ce8ff5dfb3",
+    "url": "/react-material-ui/index.html"
+  },
+  {
+    "revision": "e7418d30ea0dd64aa46f",
+    "url": "/react-material-ui/static/js/2.6ec234c0.chunk.js"
+  },
+  {
+    "revision": "2732a7511d36cc06ac5d",
+    "url": "/react-material-ui/static/js/main.ef5896e8.chunk.js"
+  },
+  {
+    "revision": "3064aa741d887559dbbf",
+    "url": "/react-material-ui/static/js/runtime~main.d1d13c8a.js"
+  },
+  {
+    "revision": "26e547dff2c31ea40d1e7d56693f127b",
+    "url": "/react-material-ui/static/media/card-1.26e547df.jpg"
+  },
+  {
+    "revision": "d156a77b9c7ca4026b812c8abbb056dd",
+    "url": "/react-material-ui/static/media/card-2.d156a77b.jpg"
+  },
+  {
+    "revision": "a081cbdb1d781ffd39915465613470c0",
+    "url": "/react-material-ui/static/media/card-3.a081cbdb.jpg"
+  },
+  {
+    "revision": "fc2a7fa845709986fd7a825c53614111",
+    "url": "/react-material-ui/static/media/header_back-1.fc2a7fa8.jpg"
+  }
+]);
